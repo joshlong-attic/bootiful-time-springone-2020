92cec2c043818543e881431d4eb996b88682baf3
Mario Gray <mario.gray@gmail.com>
2020-08-30 18:21:47 -0700

    contract messaging bones;

---
+++ contract-messaging-demo/src/main/java/com/demo/contractmessagingdemo/ContractMessagingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.contractmessagingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractMessagingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractMessagingDemoApplication.class, args);
+	}
+
+}
---
+++ contract-messaging-demo/src/test/java/com/demo/contractmessagingdemo/ContractMessagingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.contractmessagingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractMessagingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
