1e4ce14553344112afb1f1c7d2b2f7f528793265
Mario Gray <mario.gray@gmail.com>
2020-08-30 23:03:50 -0700

    scaffholding for tests

--- contract-messaging-demo/src/main/java/com/demo/contractmessagingdemo/ContractMessagingDemoApplication.java
+++ contract-messaging-demo/src/main/java/com/demo/contractmessagingdemo/ContractMessagingDemoApplication.java
@@ -2,12 +2,22 @@ package com.demo.contractmessagingdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class ContractMessagingDemoApplication {
 
+	@Bean
+	public Supplier<String> timeSupplier() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		return () -> sdf.format(new Date());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ContractMessagingDemoApplication.class, args);
 	}
-
 }
--- contract-messaging-demo/src/test/java/com/demo/contractmessagingdemo/ContractMessagingDemoApplicationTests.java
+++ contract-messaging-demo/src/test/java/com/demo/contractmessagingdemo/ContractMessagingDemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.demo.contractmessagingdemo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.MessageVerifier;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.text.SimpleDateFormat;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMessageVerifier
 @SpringBootTest
 class ContractMessagingDemoApplicationTests {
+
+	@Autowired
+	private MessageVerifier verifier;
+
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+	boolean received() {
+		return true;
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
